a66f7e2cedb9eb7e265d28afd2af3ae44d938120
Gustavo M Passone <gmpassone@gmail.com>
2025-04-04 15:28:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ function tabuada() {
     const numero = document.getElementById("numero").value;
     const vezes = document.getElementById("vezes").value;
     var calculos = document.querySelector(".calculos");
-    var titulo = document.querySelector(".titulo__content")
+    var titulo = document.querySelector(".titulo__content");
 
     calculos.innerHTML = "";
 
-    for(let i = 1; i <= vezes; i++) {
-        calculos.innerHTML += `<p>${numero} x ${i} = ${numero * i}</p>`
+    if (numero == "") {
+        titulo.innerHTML = "Digite um número!";
+        titulo.style.color = "#da1b1b";
+    } else if (vezes == "") {
+        titulo.innerHTML = "Digite o número final da tabuada!";
+        titulo.style.color = "#da1b1b";
+    } else {
+        for(let i = 1; i <= vezes; i++) {
+            calculos.innerHTML += `<p>${numero} x ${i} = ${numero * i}</p>`
+        }
+
+        titulo.innerHTML = (`Tabuada do ${numero}`);
     }
 
-    titulo.innerHTML = (`Tabuada do ${numero}`)
 }
 
 btn.addEventListener("click", tabuada)
