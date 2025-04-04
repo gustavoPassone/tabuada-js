769628852c3d8b420528326d23096a71ea7dd865
Gustavo M Passone <gmpassone@gmail.com>
2025-04-04 15:38:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,20 +7,17 @@ function tabuada() {
     var titulo = document.querySelector(".titulo__content");
 
     calculos.innerHTML = "";
-
+    
     if (numero == "") {
         titulo.innerHTML = "Digite um número!";
-        titulo.style.color = "#da1b1b";
     } else if (vezes == "") {
         titulo.innerHTML = "Digite o número final da tabuada!";
-        titulo.style.color = "#da1b1b";
     } else {
         for(let i = 1; i <= vezes; i++) {
             calculos.innerHTML += `<p>${numero} x ${i} = ${numero * i}</p>`
         }
         
         titulo.innerHTML = (`Tabuada do ${numero}`);
-        titulo.style.color = "#b1b1b1;";
     }
 
 }
